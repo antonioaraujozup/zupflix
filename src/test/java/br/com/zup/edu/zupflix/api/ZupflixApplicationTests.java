01bae12eeae673ff6ac837e967aca0bbdd0b9c50
Antonio Eloy <101589161+antonioaraujozup@users.noreply.github.com>
2022-04-08 11:25:18 -0300

    Commit inicial

---
+++ src/test/java/br/com/zup/edu/zupflix/api/ZupflixApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.edu.zupflix.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZupflixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
